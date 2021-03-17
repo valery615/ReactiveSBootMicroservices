549d545baf89a769d3afa674f7a5672645515d00
vborys <valeriy.borys@gmail.com>
2021-03-17 12:44:05 +0300

    initial commit

---
+++ src/main/java/com/softeq/reactive/entity/Message.java
@@ -0,0 +1,38 @@
+package com.softeq.reactive.entity;
+
+
+import java.util.Objects;
+
+public class Message {
+    int id;
+    String text;
+
+    public Message(int id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+            "id=" + id +
+            ", text='" + text + '\'' +
+            '}';
+    }
+}
